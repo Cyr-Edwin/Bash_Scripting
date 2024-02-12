8111db9f228ac3f00c5f6ffc8d74a2d7f603efc0
pyEdwin <cyredwin1@gmail.com>
2024-02-12 05:27:31 -0500

    add checked_argument.sh file

--- checked_arguments/checked_argument.sh
+++ checked_arguments/checked_argument.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # check if the argument is a File
-if [ -f "$1" ]
+if [[ -f "$1" ]]
 then 
      echo "The given argument is a file"
      # display the content of the file
      sleep 3
      cat $1
 # check if the argument is a Directory
-elif [ -d "$1" ]
+elif [[ -d "$1" ]]
 then
      echo "The given argument is a directory"
      # display the content of the directory
