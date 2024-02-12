32001973187a6cbe178c8cf2b1b094228cea14fa
pyEdwin <cyredwin1@gmail.com>
2024-02-12 05:22:16 -0500

    add checked_argument.sh file

--- checked_arguments/checked_argument.sh
+++ checked_arguments/checked_argument.sh
@@ -7,7 +7,6 @@ then
      # display the content of the file
      sleep 3
      cat $1
-
 # check if the argument is a Directory
 elif [ -d "$1" ]
 then
@@ -15,8 +14,6 @@ then
      # display the content of the directory
      sleep 3
      ls -l $1
-
 else
      echo "The argument is neither a file or directory"
-
 fi
